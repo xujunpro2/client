5c522e804389733bcd27ebaf6404d961971d378b
xujunpro <568106612@qq.com>
2020-04-24 10:42:26 +0800

    开始DND

---
+++ src/assets/BasicElements.js
@@ -0,0 +1,30 @@
+/*
+  Basic elements (HTML5 Elements):
+  - PARENT -> egglement, [containegg]
+ */
+const basicElements = [
+  // DIV
+  {
+    'name': 'div',
+    'type': 'div',
+    'egglement': true,
+    'containegg': true,
+    'width': 100,
+    'height': 100,
+    'attrs': {
+      'hidden': false
+    },
+    'classes': {},
+    'styles': {
+      'overflow': 'hidden',
+      'border-color': 'rgba(0, 0, 0, 0.15)',
+      'border-style': 'solid',
+      'border-width': '1px'
+    },
+    'children': []
+  }
+
+  
+]
+
+export default basicElements
